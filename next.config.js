edc08de4c976084f2163b237369ca22742a5ac45
Lia Freitas Lobo <lia.lobo@gmail.com>
2023-06-30 22:17:19 -0300

    feat: add project configs

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd,
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  // next.js config
+});
